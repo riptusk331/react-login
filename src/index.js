6848a06d267325172dc45294153741a9af595732
Ryan Joyce <ryan.joyce.88@gmail.com>
2020-10-29 14:09:40 -0400

    remove unnecessary exports

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import UserMixin from "./utils/user-mixin";
-import { setRememberCookie, getRememberCookie } from "./services/cookie";
-import { logIn, loginBasic, requestBearer } from "./services/login";
 import { useLogin } from "./hooks/useLogin";
 import { LoginProvider, LoginContext } from "./hooks/provider";
+import { getRememberCookie } from "./services/cookie"
 
 export {
-  UserMixin,
-  setRememberCookie,
-  getRememberCookie,
-  logIn,
-  loginBasic,
-  requestBearer,
   useLogin,
   LoginProvider,
   LoginContext,
+  getRememberCookie
 };
